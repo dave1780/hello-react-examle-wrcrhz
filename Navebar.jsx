84a46cc0790d56f817c4977428c82e2727619495
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-01-04 12:18:32 +0545

    1 file modified

--- Navebar.jsx
+++ Navebar.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import Home from "./Components/Home"
 import Menu from "./Components/Menu"
 import About from "./Components/About"
+
 const NaveBar=()=>{
   return(
    <>
    <nav class="nav">
-     <li><a href="/Home">Home</a>
+     <li><a href="./Components/Home">Home</a>
      <a href="/Menu ">Menu</a>
      <a href= "/About">About</a></li>
    </nav>
