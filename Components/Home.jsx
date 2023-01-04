7133e26530c44ac986febbe8945b00a72b4f8312
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-01-04 12:15:09 +0545

    3 files modified

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -3,7 +3,8 @@ import React from "react"
 export default function Home_page(){
   return (
     <>
-    
+    <h1>this is home page
+    </h1>
     </>
   )
 }
--- Components/Menu.jsx
+++ Components/Menu.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function MenuPage() {
 return(
   <>
-  
+  <h1>this is the Menu</h1>
   </>
 )
 }
